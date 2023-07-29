1a5239e997f9560790e9971d3a3595a1304523d6
Siwei Mu <msw0629@gmail.com>
2023-07-29 15:24:15 +1000

    Fixed bug

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Musiwei\UserInvitation\Http\Controllers\UserInvitationsController;
 
 Route::prefix(config('user-invitation.route.prefix'))
     ->middleware(config('user-invitation.route.middleware'))
     ->name(config('user-invitation.route.name'))
     ->group(function () {
-        Route::post('/invite', [config('user-invitation.controller'), 'invite']);
-        Route::get('/accept-invitation/{token}', [config('user-invitation.controller'), 'accept']);
-        Route::post('/register', [config('user-invitation.controller'), 'register']);
+        Route::post('/invite', [config('user-invitation.controller'), 'invite'])->name('invite');
+        Route::get('/accept-invitation/{token}', [config('user-invitation.controller'), 'accept'])->name('accept');
+        Route::post('/register', [config('user-invitation.controller'), 'register'])->name('register');
     });
