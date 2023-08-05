ca517210e7b2c5e2c79cf592f3959c4e72e5ed76
Siwei Mu <msw0629@gmail.com>
2023-08-05 15:52:57 +1000

    Fixed email subject translation bug and config serialization bug

--- src/Notifications/InvitationNotification.php
+++ src/Notifications/InvitationNotification.php
@@ -42,7 +42,7 @@ class InvitationNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject(__('user-invitation.subject'))
+            ->subject(__('user-invitation.subject', ['name' => config('app.name')]))
             ->greeting(__('user-invitation.welcome'))
             ->line(__('user-invitation.invited', ['name' => config('app.name')]))
             ->action(__('user-invitation.accept'), $this->notificationUrl)
